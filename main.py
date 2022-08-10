d34c43581744de71a843e267d569f606f9a92a39
adavidson99 <99695975+adavidson99@users.noreply.github.com>
2022-08-10 18:55:14 +1200

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,23 @@ import time
 from telegram_bot import Telegram
 from apscheduler.schedulers.background import BackgroundScheduler
 
+tickers = ['A', 'AAL', 'AAP', 'AAPL', 'ABBV', 'ABC', 'ABMD', 'ABT', 'ACN', 'ADBE', 'ADI', 'ADM', 'ADP', 'ADSK', 'AEE',
+           'AEP', 'AES', 'AFL', 'AIG', 'AIV', 'AIZ', 'AJG', 'AKAM', 'ALB', 'ALGN', 'ALK', 'ALL', 'ALLE', 'AMAT',
+           'AMCR', 'AMD', 'AME', 'AMGN', 'AMP', 'AMT', 'AMZN', 'ANET', 'ANSS', 'AON', 'AOS', 'APA', 'APD',
+           'APH', 'APTV', 'ARE', 'ATO', 'ATVI', 'AVB', 'AVGO', 'AVY', 'AWK', 'AXP', 'AZO', 'BA', 'BAC', 'BAX', 'BBY',
+           'BDX', 'BEN', 'BIIB', 'BIO', 'BK', 'BKNG', 'BKR', 'BLK', 'BMY', 'BR', 'BSX', 'BWA',
+           'BXP', 'C', 'CAG', 'CAH', 'CARR', 'CAT', 'CB', 'CBOE', 'CBRE', 'CCI', 'CCL', 'CDNS', 'CDW', 'CE',
+           'CF', 'CFG', 'CHD', 'CHRW', 'CHTR', 'CI', 'CINF', 'CL', 'CLX', 'CMA']
 
+
+# have csv file for the stocks held so if program goes down then the information will still be there
 class Stocks:
 
-    def __init__(self):
-        self.tickers = ['AAPL', 'MSFT', 'AMD', 'GOOGL', 'F']  # temporarily 5 stocks will be all 500
+    def __init__(self, held):
+        self.tickers = tickers
         self.ticker_list = []
-        self.held = {}
+        self.held = held
+        self.count = 0
         self.waitingBuy = {}  # company and recommended price of the stock to buy at
         self.waitingSell = {}  # company and recommended price of the stock to sell at
 
@@ -24,9 +34,32 @@ class Stocks:
         print(f"buy {stock} at {price}")
 
     def buy_company(self, stock, recommended_price):
-        price = int(input(f"Price {stock} bought at? "))
+        price = input(f"Price {stock} bought at? ")
+        try:
+            price = float(price)
+        except ValueError:
+            print("Error, Try again")
+            self.buy_company(stock, recommended_price)  # have maximum price instead of recommended
         self.held[stock] = price
-        price_difference = recommended_price - price
+        df = pd.read_csv('sp_500_stocks.csv')
+        df.loc[df['Ticker'] == stock, 'Price'] = price
+        df.loc[df['Ticker'] == stock, 'Held'] = 1
+        df.loc[df['Ticker'] == stock, 'Profit'] -= price
+        df.to_csv('sp_500_stocks.csv')
+
+    def sell_company(self, stock, recommended_price):
+        price = input(f"Price {stock} sold at? ")
+        try:
+            price = float(price)
+        except ValueError:
+            print("Error, Try again")
+            self.sell_company(stock, recommended_price)
+        del self.held[stock]
+        df = pd.read_csv('sp_500_stocks.csv')
+        df.loc[df['Ticker'] == stock, 'Price'] = 0
+        df.loc[df['Ticker'] == stock, 'Held'] = 0
+        df.loc[df['Ticker'] == stock, 'Profit'] += price
+        df.to_csv('sp_500_stocks.csv')
 
     def remove_company(self, stock, price):
         self.waitingSell[stock] = price
@@ -36,27 +69,25 @@ class Stocks:
 
     def check_waiting_list(self):
         if self.waitingSell:
-            for stock, price in self.waitingSell:
+            for stock, price in self.waitingSell.items():
+                self.count += 1
                 self.sell_company(stock, price)
+                if self.count == len(self.waitingSell):
+                    self.waitingBuy = {}
+                    self.count = 0
         if self.waitingBuy:
-            for stock, price in self.waitingBuy:
+            for stock, price in self.waitingBuy.items():
+                self.count += 1
                 self.buy_company(stock, price)
-
-    def sell_company(self, stock, recommended_price):
-        price = input(f"Price {stock} sold at? ")
-        try:
-            price = float(price)
-        except ValueError:
-            print("Error, Try again")
-            self.sell_company(stock, recommended_price)
-        self.held[stock] = price
-        price_difference = recommended_price - price
+                if self.count == len(self.waitingBuy):
+                    self.waitingBuy = {}
+                    self.count = 0
 
     def refresh_program(self):
         self.update_tickers()
 
         for stock, trend, rsi, price in self.ticker_list:
-            if trend > 0 and rsi < 30 and not self.held[stock]:
+            if trend > 0 and rsi < 30 and stock not in self.held:
                 self.add_company(stock, price)
 
             if stock in self.held and rsi > 70:
@@ -106,10 +137,8 @@ def rsi_calculation(file):
     df.to_csv(file)
 
 
-"""
 def holding_data():
-    holding_list = []
-    tickers.sort()
+    holding_list = {}
     p = Path('C:\\Users\\adamg\\PycharmProjects\\ticker')
     datafile = p / 'sp_500_stocks.csv'
     if datafile.exists():
@@ -123,22 +152,28 @@ def holding_data():
                 name = row['Ticker']
                 held = int(row['Held'])
                 price = float(row['Price'])
-                holding_list.append((name, held, price))
+                if held == 1:
+                    holding_list[name] = price
 
     return holding_list
-"""
 
 
 def main():
-    s = Stocks()
+    currently_held = holding_data()
+    s = Stocks(currently_held)
     s.ticker_data()
     s.refresh_program()
+
+    print(s.waitingBuy)
+    print(s.waitingSell)
+    print(s.ticker_list)
+    print(s.held)
 
     sched = BackgroundScheduler()
     sched.configure(timezone='US/Eastern')
     sched.add_job(s.refresh_program, trigger='cron', day_of_week='mon-fri',
-                  hour='1-4',
-                  minute='*/2')
+                  hour='9-16',
+                  minute='*/15')
     sched.start()
 
     while True:
