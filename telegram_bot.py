7423e1f82c7361643384f07f2a6468841f317610
adavidson99 <99695975+adavidson99@users.noreply.github.com>
2022-08-10 12:26:10 +1200

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -8,15 +8,15 @@ class Telegram:
 
     def __init__(self):
         self.api_id = 15901021
-        self.api_hash = '55bf836ec7e1856d1753e00e0513a303'
-        self.token = '5401109253:AAF5vSLfJV2rjhH9JiQ-j7fgbS7VIKffTN4'
-        self.phone = '+64212150997'
+        self.api_hash = ''
+        self.token = ''
+        self.phone = ''
         self.client = TelegramClient('session', self.api_id, self.api_hash)
 
     def send(self, message):
         self.client.connect()
         try:
-            receiver = InputPeerUser(5330092176, 0)
+            receiver = InputPeerUser(, 0)
             self.client.send_message(receiver, message, parse_mode='html')
         except Exception as e:
             print(e)
