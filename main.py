bea0bec44e7536b0466b7651b0b5554472302cb1
adavidson99 <99695975+adavidson99@users.noreply.github.com>
2022-08-10 12:24:23 +1200

    Update main.py

--- main.py
+++ main.py
@@ -3,25 +3,101 @@ from pathlib import Path
 import csv
 import pandas as pd
 import pandas_ta as ta
-from apscheduler.schedulers.blocking import BlockingScheduler
+import time
+from telegram_bot import Telegram
+from apscheduler.schedulers.background import BackgroundScheduler
 
 
-tickers = ['AAPL', 'MSFT', 'AMD', 'GOOGL', 'F']   # temporarily 5 stocks will be all 500
+class Stocks:
 
+    def __init__(self):
+        self.tickers = ['AAPL', 'MSFT', 'AMD', 'GOOGL', 'F']  # temporarily 5 stocks will be all 500
+        self.ticker_list = []
+        self.held = {}
+        self.waitingBuy = {}  # company and recommended price of the stock to buy at
+        self.waitingSell = {}  # company and recommended price of the stock to sell at
 
-def buy(data, holding_list, index):
-    """place the order to buy the stock"""
-    holding_list[index] = (holding_list[index][0], 1, data[3])  # shows the equity is now held and price it was bought
-    print(f'buy {data}')
-    print(holding_list)
-    return holding_list
+    def add_company(self, stock, price):
+        self.waitingBuy[stock] = price
+        telegram = Telegram()
+        telegram.send(f"buy {stock} at {price}")
+        print(f"buy {stock} at {price}")
 
+    def buy_company(self, stock, recommended_price):
+        price = int(input(f"Price {stock} bought at? "))
+        self.held[stock] = price
+        price_difference = recommended_price - price
 
-def sell(data, holding_list, index):
-    """sell the already held order on a stock"""
-    holding_list[index] = (holding_list[index][0], 0, data[3])  # shows the equity is now sold and price sold at
-    print(f'sell {data}')
-    return holding_list
+    def remove_company(self, stock, price):
+        self.waitingSell[stock] = price
+        telegram = Telegram()
+        telegram.send(f"sell {stock} at {price}")  # to do: calculate the lowest price to sell / max price to buy
+        print(f"sell {stock} at {price}")
+
+    def check_waiting_list(self):
+        if self.waitingSell:
+            for stock, price in self.waitingSell:
+                self.sell_company(stock, price)
+        if self.waitingBuy:
+            for stock, price in self.waitingBuy:
+                self.buy_company(stock, price)
+
+    def sell_company(self, stock, recommended_price):
+        price = input(f"Price {stock} sold at? ")
+        try:
+            price = float(price)
+        except ValueError:
+            print("Error, Try again")
+            self.sell_company(stock, recommended_price)
+        self.held[stock] = price
+        price_difference = recommended_price - price
+
+    def refresh_program(self):
+        self.update_tickers()
+
+        for stock, trend, rsi, price in self.ticker_list:
+            if trend > 0 and rsi < 30 and not self.held[stock]:
+                self.add_company(stock, price)
+
+            if stock in self.held and rsi > 70:
+                self.remove_company(stock, price)
+
+            if stock in self.held and (self.held[stock] - (self.held[stock] / 10)) > price:
+                # if equity is being held and the current price is 10% less than the buy price
+                self.remove_company(stock, price)
+
+        print("refreshed")
+
+    def update_tickers(self):
+        yf.pdr_override()
+        self.tickers.sort()
+        for ticker in self.tickers:
+            data = yf.download(ticker, group_by='Ticker', period="1mo", interval='1h')
+            data['Ticker'] = ticker
+            data.to_csv(f'ticker_{ticker}.csv')
+
+    def ticker_data(self):
+        p = Path('C:\\Users\\adamg\\PycharmProjects\\ticker')
+        files = p.glob('ticker_*.csv')
+        for file in files:
+            rsi_calculation(file)
+
+            with open(file, 'r') as _filehandler:
+                csv_file_reader = csv.DictReader(_filehandler)
+                rownum = 0
+
+                for row in csv_file_reader:
+
+                    if rownum == 1:
+                        start = float(row['Open'])
+
+                    rownum += 1
+
+                end = float(row['Close'])
+                rsi = float(row['RSI'])
+                name = row['Ticker']
+                trend = end - start
+                self.ticker_list.append((name, trend, rsi, end))
 
 
 def rsi_calculation(file):
@@ -30,6 +106,7 @@ def rsi_calculation(file):
     df.to_csv(file)
 
 
+"""
 def holding_data():
     holding_list = []
     tickers.sort()
@@ -49,85 +126,25 @@ def holding_data():
                 holding_list.append((name, held, price))
 
     return holding_list
-
-
-def ticker_data():
-    ticker_list = []
-    p = Path('C:\\Users\\adamg\\PycharmProjects\\ticker')
-    files = p.glob('ticker_*.csv')
-    for file in files:
-
-        rsi_calculation(file)
-
-        with open(file, 'r') as _filehandler:
-
-            csv_file_reader = csv.DictReader(_filehandler)
-            rownum = 0
-
-            for row in csv_file_reader:
-
-                if rownum == 1:
-                    start = float(row['Open'])
-
-                rownum += 1
-
-            end = float(row['Close'])
-            rsi = float(row['RSI'])
-            name = row['Ticker']
-            trend = end - start
-            ticker_list.append((name, trend, rsi, end))
-
-    return ticker_list
-
-
-def update_tickers():
-    yf.pdr_override()
-
-    tickers.sort()
-    for ticker in tickers:
-        data = yf.download(ticker, group_by='Ticker', period="1mo", interval='1h')
-        data['Ticker'] = ticker
-        data.to_csv(f'ticker_{ticker}.csv')
-
-
-def refresh_program(ticker_list, holding_list):
-
-    for index, data in enumerate(ticker_list):
-
-        if data[1] > 0 and data[2] < 30 and holding_list[index][0] == 0:
-            holding_list = buy(data, holding_list, index)
-
-        if holding_list[index][0] == 1 and data[2] > 70:
-            holding_list = sell(data, holding_list, index)
-
-        if holding_list[index][0] == 1 and (holding_list[index][1] - (holding_list[index][1] / 10)) > data[3]:
-            # if equity is being held and the current price is 10% less than the buy price
-            holding_list = sell(data, holding_list, index)
-
-    update_tickers()
+"""
 
 
 def main():
+    s = Stocks()
+    s.ticker_data()
+    s.refresh_program()
 
-    update_tickers()
-    ticker_list = ticker_data()
-    holding_list = holding_data()
-
-    print(ticker_list)
-    print(holding_list)
-
-    refresh_program(ticker_list, holding_list)
-
-    sched = BlockingScheduler()
+    sched = BackgroundScheduler()
     sched.configure(timezone='US/Eastern')
-    sched.add_job(refresh_program, trigger='cron', args=[ticker_list, holding_list], day_of_week='mon-fri', hour='9-16',
-                  minute='*/15')
+    sched.add_job(s.refresh_program, trigger='cron', day_of_week='mon-fri',
+                  hour='1-4',
+                  minute='*/2')
     sched.start()
+
+    while True:
+        s.check_waiting_list()
+        time.sleep(60)
 
 
 if __name__ == '__main__':
     main()
-
-"""
-ADD ANOTHER JOB SCHEDULE TO UPDATE THE TICKERS ABOUT A MINUTE BEFORE REFRESHING THE PROGRAM
-"""
