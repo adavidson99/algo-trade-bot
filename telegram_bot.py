6afc35b4b30ab817ae27432654412f1ad54307b1
Adam Davidson <adam99davidson@gmail.com>
2022-08-17 15:54:13 +1200

    modified:   telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -7,16 +7,16 @@ from telethon import TelegramClient, sync, events
 class Telegram:
 
     def __init__(self):
-        self.api_id = 15901021
-        self.api_hash = ''
-        self.token = ''
-        self.phone = ''
+        self.api_id = 000000 # api id
+        self.api_hash = 'api_hash'
+        self.token = 'api_token'
+        self.phone = 'phone_number'
         self.client = TelegramClient('session', self.api_id, self.api_hash)
 
     def send(self, message):
         self.client.connect()
         try:
-            receiver = InputPeerUser(, 0)
+            receiver = InputPeerUser(#user_id, 0)
             self.client.send_message(receiver, message, parse_mode='html')
         except Exception as e:
             print(e)
