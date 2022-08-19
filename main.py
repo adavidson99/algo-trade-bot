08980277c4633064e58f8cbd72dd7a92784fc7c1
adavidson99 <99695975+adavidson99@users.noreply.github.com>
2022-08-19 14:35:37 +1200

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import pandas as pd
 import pandas_ta as ta
 import time
 import requests
+from pytimedinput import timedInput
 import bs4 as bs
 from telegram_bot import Telegram
-from apscheduler.schedulers.background import BackgroundScheduler
 
 
 # have csv file for the stocks held so if program goes down then the information will still be there
@@ -20,6 +20,7 @@ class Stocks:
         self.count = 0
         self.waitingBuy = {}  # company and recommended price of the stock to buy at
         self.waitingSell = {}  # company and recommended price of the stock to sell at
+        self.timer = time.time()
 
     def add_company(self, stock, price):
         self.waitingBuy[stock] = price
@@ -28,39 +29,49 @@ class Stocks:
         print(f"buy {stock} at {price}")
 
     def buy_company(self, stock, recommended_price):
-        price = input(f"Price {stock} bought at? ")
-        try:
-            price = float(price)
-        except ValueError:
-            print("Error, Try again")
-            self.buy_company(stock, recommended_price)  # have maximum price instead of recommended
-        self.held[stock] = price
-        df = pd.read_csv('nzx_50_stocks.csv')
-        df.loc[df['Ticker'] == stock, 'Price'] = price
-        df.loc[df['Ticker'] == stock, 'Held'] = 1
-        df.loc[df['Ticker'] == stock, 'Profit'] -= price
-        df.to_csv('nzx_50_stocks.csv')
-        print(f"Bought {stock} at {price}")
+        price, timed_out = timedInput(f"Price {stock} bought at? ", timeout=30)
+        if not timed_out:
+            shares = input(f"Number of shares? ")
+            try:
+                price = float(price)
+                shares = float(shares)
+            except ValueError:
+                print("Error, not a valid value")
+                self.buy_company(stock, recommended_price)
+            self.held[stock] = (price, shares)
+            df = pd.read_csv('nzx_50_stocks.csv')
+            df.loc[df['Ticker'] == stock, 'Price'] = price
+            df.loc[df['Ticker'] == stock, 'Held'] = 1
+            df.loc[df['Ticker'] == stock, 'Shares'] = shares
+            df.loc[df['Ticker'] == stock, 'Profit'] -= (price * shares)
+            df.to_csv('nzx_50_stocks.csv')
+            print(f"Bought {shares} shares of {stock} at {price}")
 
     def sell_company(self, stock, recommended_price):
-        price = input(f"Price {stock} sold at? ")
-        try:
-            price = float(price)
-        except ValueError:
-            print("Error, Try again")
-            self.sell_company(stock, recommended_price)
-        del self.held[stock]
-        df = pd.read_csv('sp_500_stocks.csv')
-        df.loc[df['Ticker'] == stock, 'Price'] = 0
-        df.loc[df['Ticker'] == stock, 'Held'] = 0
-        df.loc[df['Ticker'] == stock, 'Profit'] += price
-        df.to_csv('nzx_50_stocks.csv')
-        print(f"Sold {stock} at {price}")
+        price, timed_out = timedInput(f"Price {stock} sold at? ", timeout=30)
+        if not timed_out:
+            bought_price = self.held[stock][0]
+            shares = self.held[stock][1]
+            try:
+                price = float(price)
+                shares = float(shares)
+            except ValueError:
+                print("Error, Try again")
+                self.sell_company(stock, recommended_price)
+            del self.held[stock]
+            df = pd.read_csv('nzx_50_stocks.csv')
+            df.loc[df['Ticker'] == stock, 'Price'] = 0
+            df.loc[df['Ticker'] == stock, 'Held'] = 0
+            df.loc[df['Ticker'] == stock, 'Shares'] = 0
+            df.loc[df['Ticker'] == stock, 'Profit'] += (price * shares)
+            df.loc[df['Ticker'] == stock, 'Pct_dif'] += ((price - bought_price) / bought_price) * 100
+            df.to_csv('nzx_50_stocks.csv')
+            print(f"Sold {stock} at {price}")
 
     def remove_company(self, stock, price):
         self.waitingSell[stock] = price
-        telegram = Telegram()
-        telegram.send(f"sell {stock} at {price}")  # to do: calculate the lowest price to sell / max price to buy
+        # telegram = Telegram()
+        # telegram.send(f"sell {stock} at {price}")  # to do: calculate the lowest price to sell / max price to buy
         print(f"sell {stock} at {price}")
 
     def check_waiting_list(self):
@@ -71,6 +82,7 @@ class Stocks:
                 if self.count == len(self.waitingSell):
                     self.waitingBuy = {}
                     self.count = 0
+
         if self.waitingBuy:
             for stock, price in self.waitingBuy.items():
                 self.count += 1
@@ -90,41 +102,47 @@ class Stocks:
             if stock in self.held and rsi > 70:
                 self.remove_company(stock, price)
 
-            if stock in self.held and (self.held[stock] - (self.held[stock] / 10)) > price:
+            if stock in self.held and (self.held[stock][0] - (self.held[stock][0] / 10)) > price:
                 # if equity is being held and the current price is 10% less than the buy price
                 self.remove_company(stock, price)
 
-        print(f"updated_stocks: {self.ticker_list}")
+            # if stock in self.held and self.held[stock][2] (days held) > 30:
+                # self.remove_company(stock, price)
+
+        print(f"held stocks: {self.held}")
+        self.check_waiting_list()
 
     def update_tickers(self):
         yf.pdr_override()
         for ticker in self.tickers:
             data = yf.download(ticker, group_by='Ticker', period="1mo", interval='1h')
-            data['Ticker'] = ticker
-            data.to_csv(f'ticker_{ticker}.csv')
+            if not data.empty:
+                data['Ticker'] = ticker
+                data.to_csv(f'ticker_{ticker}.csv')
 
     def ticker_data(self):
-        p = Path('C:\\Users\\adamg\\algo-trade-bot')
+        p = Path('C:\\Users\\adamg\\ticker')
         files = p.glob('ticker_*.csv')
         for file in files:
             rsi_calculation(file)
-
             with open(file, 'r') as _filehandler:
                 csv_file_reader = csv.DictReader(_filehandler)
                 rownum = 0
-
                 for row in csv_file_reader:
-
                     if rownum == 1:
                         start = float(row['Open'])
 
                     rownum += 1
 
-                end = float(row['Close'])
-                rsi = float(row['RSI'])
-                name = row['Ticker']
-                trend = end - start
-                self.ticker_list.append((name, trend, rsi, end))
+                try:
+                    end = float(row['Close'])
+                    rsi = float(row['RSI'])
+                    name = row['Ticker']
+                    trend = end - start
+                    self.ticker_list.append((name, trend, rsi, end))
+
+                except ValueError:
+                    pass
 
 
 def rsi_calculation(file):
@@ -143,8 +161,9 @@ def holding_data():
             name = row['Ticker']
             held = int(row['Held'])
             price = float(row['Price'])
+            shares = float(row['Shares'])
             if held == 1:
-                holding_list[name] = price
+                holding_list[name] = (price, shares)
 
     return holding_list
 
@@ -159,7 +178,7 @@ def update_ticker_file(tickers):
         with open(datafile, 'r', newline='') as csvfile:
             info = csv.reader(csvfile, delimiter='\t')
             for row in info:
-                ticker = row[0].split(',')[0]
+                ticker = row[0].split(',')[1]
                 current_tickers.add(ticker)
 
             csvfile.close()
@@ -185,39 +204,37 @@ def update_ticker_file(tickers):
             csvfile.close()
 
 
-def save_sp500_tickers():
-    resp = requests.get('https://en.wikipedia.org/wiki/S%26P/NZX_50_Index')
-    soup = bs.BeautifulSoup(resp.text, 'lxml')
-    table = soup.find('table', {'class': 'wikitable sortable'})
+def save_tickers():
     tickers = []
-    for row in table.findAll('tr')[1:]:
+    nz = requests.get('https://en.wikipedia.org/wiki/S%26P/NZX_50_Index')
+    nz_soup = bs.BeautifulSoup(nz.text, 'lxml')
+    nz_table = nz_soup.find('table', {'class': 'wikitable sortable'})
+    for row in nz_table.findAll('tr')[1:]:
         ticker = row.findAll('td')[0].text
-        tickers.append(ticker)
+        nz_ticker = ticker.replace("\n", "")
+        tickers.append(nz_ticker)
 
-    result = [s.replace("\n", "") for s in tickers]
+    aus = requests.get('https://en.wikipedia.org/wiki/S%26P/ASX_200')
+    aus_soup = bs.BeautifulSoup(aus.text, 'lxml')
+    aus_table = aus_soup.find('table', {'class': 'wikitable sortable'})
+    for row in aus_table.findAll('tr')[1:]:#
+        ticker = row.findAll('td')[0].text
+        aus_ticker = ticker.replace("\n", ".AX")
+        tickers.append(aus_ticker)
 
-    return set(result)
+    return set(tickers)
 
 
 def main():
-    tickers = save_sp500_tickers()
+    tickers = save_tickers()
     update_ticker_file(tickers)
     currently_held = holding_data()
     s = Stocks(currently_held, tickers)
-    s.refresh_program()
-
-    print(s.held)
-
-    sched = BackgroundScheduler()
-    sched.configure(timezone='US/Eastern')
-    sched.add_job(s.refresh_program, trigger='cron', day_of_week='mon-fri',
-                  hour='1-23',
-                  minute='*/5')
-    sched.start()
 
     while True:
-        s.check_waiting_list()
-        time.sleep(60)
+        # refresh every 5 minutes
+        s.refresh_program()
+        time.sleep(300)
 
 
 if __name__ == '__main__':
